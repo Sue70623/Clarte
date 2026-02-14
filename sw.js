862d50928eaa0eabc3aa8f3a0d11f3c9e1777eaa
Sue70623 <suzannefv7@gmail.com>
2026-02-14 19:33:01 +0100

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,37 @@
+// sw.js  
+const CACHE_NAME = 'clarte-v1';  
+const urlsToCache = [  
+  './',  
+  './index.html',  
+  './app.js',  
+  './manifest.json'  
+];  
+  
+self.addEventListener('install', event => {  
+  event.waitUntil(  
+    caches.open(CACHE_NAME)  
+      .then(cache => cache.addAll(urlsToCache))  
+  );  
+  self.skipWaiting();  
+});  
+  
+self.addEventListener('fetch', event => {  
+  event.respondWith(  
+    caches.match(event.request)  
+      .then(response => response || fetch(event.request))  
+  );  
+});  
+  
+self.addEventListener('activate', event => {  
+  event.waitUntil(  
+    caches.keys().then(cacheNames => {  
+      return Promise.all(  
+        cacheNames.map(cacheName => {  
+          if (cacheName !== CACHE_NAME) {  
+            return caches.delete(cacheName);  
+          }  
+        })  
+      );  
+    })  
+  );  
+});
